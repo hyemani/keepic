ce3a842b9b5063220ebceb45187aa0a79c5af3be
hyemani <hhm940308@gmail.com>
2026-09-12 15:26:03 +0900

    캘린더 메인이미지 교체

--- app/goods/page.tsx
+++ app/goods/page.tsx
@@ -35,7 +35,7 @@ const goodsTypes = [
   {
     name: "캘린더",
     desc: "매달 바뀌는 탁상용 캘린더",
-    image: "/goods/calendar/large-1.jpg",
+    image: "/goods/calendar/main-1.jpg",
     alt: "사진이 담긴 탁상용 캘린더",
     href: "/goods/calendar",
   },
